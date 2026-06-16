c5564d026d051604c27f11a0db19eaacec38da5b
Vasto <sonicdarkultra44@gmail.com>
2026-06-15 18:47:55 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,5 +6,14 @@
 // Called Every Frame //
 function drawTitleScreen() {
     // drawTitleScreen(): draws the play button and gets the players username
-    
+
+    // play button bg
+    ctx.fillStyle = "rgb(20, 200, 50)";
+    ctx.rect(cnv.width*0.35, cnv.height*0.6, cnv.width*0.3, cnv.height*0.1);
+
+    // play button text
+    ctx.fillStyle = "white";
+    ctx.font = "500 30px Outfit"
+    ctx.textAlign = "center";
+    ctx.fillText("LIVE", cnv.width*0.5, cnv.height*0.55);
 }
