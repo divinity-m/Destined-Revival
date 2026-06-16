6db6ee04951c80f651656437dbf937fb1fe7e3bf
Vasto <sonicdarkultra44@gmail.com>
2026-06-15 18:13:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //* DESTINED REVIVAL SCRIPT.JS *//
+
+// Game SetUp
+const cnv = document.getElementById("game-canvas");
+const ctx = cnv.getContext("2d");
+let gameState = "inGame";
+let firstUserInteraction = false;
+
+// Global Variables
+let now = Date.now();
+
+
+// Draw Canvas
+function draw() {
+    // draw(): draws everything in the canvas
+  
+    // On Refresh (New Frame)
+    now = Date.now();
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+
+    //
+    requestAnimationFrame();
+}
+
+draw();
