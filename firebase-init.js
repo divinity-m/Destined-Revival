ef526924ace8d96b1afd4e8611421bf7b1567d34
Vasto <sonicdarkultra44@gmail.com>
2026-06-17 10:37:23 -0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
 
-// The web app's Firebase configuration | LEAKED API KEY AIzaSyDuPa2yeylf967Z2c6ZyUi5CGn8pTO0Qn4
+// The web app's Firebase configuration
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 // Initialize Firebase
