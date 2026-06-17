268a01e01198918016489d4fff8e007ffbf45942
Vasto <sonicdarkultra44@gmail.com>
2026-06-17 10:25:48 -0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
 
-// Check if running in a CI environment with the injected variable
-const configSource = process.env.FIREBASE_CONFIG;
-
 // The web app's Firebase configuration | LEAKED API KEY AIzaSyDuPa2yeylf967Z2c6ZyUi5CGn8pTO0Qn4
-const firebaseConfig = configSource
-  ? JSON.parse(configSource)
-  : {
-      apiKey: process.env.FIREBASE_KEY,
-      authDomain: "destined-revival.firebaseapp.com",
-      projectId: "destined-revival",
-      storageBucket: "destined-revival.firebasestorage.app",
-      messagingSenderId: "955217871912",
-      appId: "1:955217871912:web:2cd980116310e4151de11d",
-      measurementId: "G-W3S0BR27TQ"
-    };
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
