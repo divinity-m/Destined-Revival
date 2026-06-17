e54604f78ca7278a7e2d31de0c7bc92778746c17
Vasto <sonicdarkultra44@gmail.com>
2026-06-16 19:56:00 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -7,6 +7,14 @@
 function drawTitleScreen() {
     // drawTitleScreen(): draws the play button and gets the players username
 
+    // title
+    ctx.fillStyle = "blue";
+    ctx.strokeStyle = "black";
+    ctx.font = "500 30px Outfit"
+    ctx.textAlign = "center";
+    ctx.fillText("DESTINED REVIVAL", cnv.width*0.5, cnv.height*0.4);
+    ctx.strokeText("DESTINED REVIVAL", cnv.width*0.5, cnv.height*0.4);
+
     // play button bg
     ctx.fillStyle = "rgb(20, 200, 50)";
     ctx.rect(cnv.width*0.35, cnv.height*0.6, cnv.width*0.3, cnv.height*0.1);
